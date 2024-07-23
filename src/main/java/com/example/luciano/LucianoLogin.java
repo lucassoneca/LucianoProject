e24b9e660a24224b61f31fe683b5aac7e8ffdda2
LUCAS BEZERRA DA CRUZ <lucasbezerracrz@gmail.com>
2024-07-23 10:27:32 -0300

    Add files via upload

--- src/main/java/com/example/luciano/LucianoLogin.java
+++ src/main/java/com/example/luciano/LucianoLogin.java
@@ -1,6 +1,9 @@
 package com.example.luciano;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LucianoLogin extends AppCompatActivity {
 
+    private TextView text_tela_cadastro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_luciano_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(LucianoLogin.this, LucianoCadastro.class);
+                startActivity(intent);
+
+            }
         });
     }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
 }
